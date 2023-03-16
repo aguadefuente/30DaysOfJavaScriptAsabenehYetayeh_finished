1fc583445cfad8cc78c21740ab171e98db40d224
lenilaurae <lenilaurae.gmail.com>
2023-03-16 17:56:09 +0000

    ultimo ejercicio del dia

--- Array/main.js
+++ Array/main.js
@@ -309,8 +309,7 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 /*Sort the array and find the min and max age
 Find the median age(one middle item or two middle items divided by two)
 Find the average age(all items divided by number of items)
-Find the range of the ages(max minus min)
-Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array 
+Find the range of the ages(max minus min) 
 */
 
 ages.sort()
@@ -337,5 +336,7 @@ let rangeAge = maxAge - minAge
 console.log("The range of the ages is:", rangeAge)
 
 //Find the middle country(ies) in the countries array
+let middleCountry = countries[countries.length/2-1]
+console.log(middleCountry)
 
 //Divide the countries array into two equal arrays if it is even. If countries array is not even , one more country for the first half.
