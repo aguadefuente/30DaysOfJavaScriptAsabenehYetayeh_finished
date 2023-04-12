06a4b6a7ce7059002027443a931bc3c23ee4e103
lenilaurae <lenilaurae.gmail.com>
2023-04-12 15:13:26 +0100

    trabajando objetos

--- functional/main2.js
+++ functional/main2.js
@@ -134,7 +134,7 @@ return array of arrays with two elementos cada uno (1 key, 2 value)
 //mas FOR-OF
 console.log("Object.entries() + with for-of");
 for (const [key, value] of Object.entries(perro)) {
-  console.log(value);
+  console.log("probando de nuevo ", value);
 }
 //////////////////
 
@@ -144,34 +144,58 @@ Object.entries(perro).forEach(([key, value]) => {
   console.log(value);
 });
 
-//
-console.log(Object.entries(names));
-console.log(Object.keys(names));
-console.log(Object.values(names));
-
-let probandosisale = Object.entries(names).sort();
-console.log(probandosisale);
-
-let probandosortear = Object.entries(names).sort((a, b) => {
-  //ascending sort
-  if (a[1] < b[1]) return 1;
-  if (a[1] > b[1]) return -1;
-  return 0;
-});
-
-console.log(probandosortear);
-
-console.log(names);
-
+////////////////////////// OTROS CHALLENGES ///////////////////////////////
+//creando un array con objetos [ { A: 1 }, { C: 1 }, { B: 2 }, { P: 1 } ] a partir de { A: 1, C: 1, B: 2, P: 1 }
 let thearr = [];
 let haciendoloobjetoarr = Object.entries(names);
 haciendoloobjetoarr.map(function (arr) {
   return thearr.push({ [arr[0]]: arr[1] }); //notaaaaa!!!! => LA KEY LA TENGO QUE ENMARCAR EN [] como vemos [arr[0]]
 });
-console.log(thearr);
+console.log("el array de objetos: ", thearr);
 
+//Pasar un array de objetos [ { A: 1 }, { C: 1 }, { B: 2 }, { P: 1 } ] a un objeto { A: 1, C: 1, B: 2, P: 1 }
 let objetoagain = {};
 for (let letter of thearr) {
   objetoagain = { ...objetoagain, ...letter };
+  //console.log(objetoagain);
+  /*esto me lo enseñó Patatin para asinarle nuevo valor al objeto se hace una copia usando ...spread-operator
+  1 vuelta - objetoagain = {...{}, ...{A:1}} => los tres puntitos rompen los {} internos y queda {A:1}
+  2 vuelta - objetoagain = {...{A:1}, ...{C:1}} => {A:1, C:1}
+  3 vuelta - objetoagain = {...{A:1, C:1}, ...{B:2}} => {A:1, C:1, B:2}...etc
+  */
 }
 console.log(objetoagain);
+
+//cuál letra está mas veces? usando el objeto { A: 1, C: 1, B: 2, P: 1 }
+let usamosSort = Object.entries(objetoagain).sort((a, b) => {
+  //ascending sort
+  if (a[1] < b[1]) return 1;
+  if (a[1] > b[1]) return -1;
+  return 0;
+});
+console.log(usamosSort);
+console.log(
+  "la letra mas usada es la " +
+    usamosSort[0][0] +
+    " - " +
+    usamosSort[0][1] +
+    " veces"
+);
+
+//sortear usando el array de objetos [ { A: 1 }, { C: 1 }, { B: 2 }, { P: 1 } ]
+console.log(thearr);
+
+const quesera = [];
+for (let elem of thearr) {
+  quesera.push(Object.keys(elem));
+}
+console.log(quesera);
+
+//console.log(thearr[0][quesera[0]]);
+
+for (let i = 0; i < thearr.length; i++) {
+  console.log(thearr[i][quesera[i]]);
+}
+
+thearr.sort((a, b) => Object.values(b)[0] - Object.values(a)[0]);
+console.log(thearr);
