31d9d112e45317dcc811b1cc4e29c9eb32f23b03
lenilaurae <lenilaurae.gmail.com>
2023-04-17 17:16:23 +0100

    mi taldito

--- day14_errorhandling/main.js
+++ day14_errorhandling/main.js
@@ -1,3 +1,22 @@
+/*Some times you will get a runtime error when you try to access 
+an undefined variable or call undefined function 
+
+try {
+  // code that may throw an error
+} catch (err) {
+  // code to be executed if an error occurs
+} finally {
+  // code to be executed regardless of an error occurs or not
+}
+
+
+TRY: wrap suspicious code that may throw an error in try block
+CATCH: used to log an error or display specific messages to the user.
+FINALLY: will always be executed regardless of the occurrence of an error. 
+The finally block can be used to complete the remaining task or reset variables
+THROW: defines a custom error. The technical term for this is: JavaScript will throw an exception (throw an error).
+*/
+
 try {
     let lastName = "Yetayeh";
     let fullName = fistName + " " + lastName;
@@ -30,17 +49,37 @@ try {
   Error message fistName is not defined
   In any case I will be executed
   */
+
+  const throwErrorExampleFun = (num) => {
+    let message
+    let x = num
+    try {
+      if (num == '') throw 'empty'
+      if (isNaN(num)) throw 'not a number'
+      num = Number(num)
+      if (num < 5) throw 'too low'
+      if (num > 10) throw 'too high'
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  console.log(throwErrorExampleFun(""))
+  console.log(throwErrorExampleFun(10))
+  console.log(throwErrorExampleFun("g"))
+
+
+///
   
-  ///Error Types
-  //ReferenceError: An illegal reference has occurred. Is thrown if we use a variable that has not been declared.
+  //////////////////////   ERROR TYPES   //////////////////////
+  //ReferenceError: An illegal reference. A variable that has not been declared.
   let firstName = "Asabeneh";
   let fullName = firstName + " " + lastName; //Uncaught ReferenceError: lastName is not defined
   console.log(fullName);
   
-  //SyntaxError: A syntax error has occurred
+  //SyntaxError: 
   let square = 2 x 2 //unexpected identifier x
   console.log(square)
   
-  //TypeError: A type error has occurred
+  //TypeError:
   let num = 10
   console.log(num.toLowerCase()) //Uncaught TypeError: num.toLowerCase is not a function
---
+++ day15_Class/main.js
@@ -0,0 +1,17 @@
+console.info("Apuntes");
+/*
+Everything in JavScript is an object. 
+We create class to create an object. 
+A Class is like an object constructor, or a "blueprint" for creating objects
+We instantiate a class to create an object
+##The class defines attributes and the behavior of the object, 
+##while the object, on the other hand, represents the class
+Creating an object from a class is called class INSTANTIATION
+*/
+
+//Defining a Class -> CamelCase and {}
+class Person {
+  // code goes here
+}
+const person = new Person(); //instantiation -> we use KEYWORD NEW
+console.log(person); //Person {}
